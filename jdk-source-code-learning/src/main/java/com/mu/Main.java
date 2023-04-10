7b87b604e252312b6508063e60a81c1b2364943c
mu-ccc <mu2781965@outlook.com>
2023-04-10 16:19:31 +0800

    init

---
+++ jdk-source-code-learning/src/main/java/com/mu/Main.java
@@ -0,0 +1,10 @@
+package com.mu;
+
+import java.util.ArrayList;
+
+public class Main {
+    public static void main(String[] args) {
+//        ArrayList
+        System.out.println("Hello world!");
+    }
+}
